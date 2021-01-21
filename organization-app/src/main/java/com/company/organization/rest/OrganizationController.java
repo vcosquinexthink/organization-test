dab86fd20bac93a3b437ffb0872cb3de5c8a7da1
Victor Cosqui <victor.cosqui@gmail.com>
2021-01-21 14:28:33 +0100

    [#0] add infra files for deployment with docker-compose and k8s, high availability with load balancer

--- organization-app/src/main/java/com/company/organization/rest/OrganizationController.java
+++ organization-app/src/main/java/com/company/organization/rest/OrganizationController.java
@@ -1,6 +1,7 @@
 package com.company.organization.rest;
 
 import com.company.organization.domain.Organization;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import static com.company.organization.rest.HierarchyRepresentation.downstreamHi
 import static com.company.organization.rest.HierarchyRepresentation.upstreamHierarchy;
 
 @RestController
+@Slf4j
 public class OrganizationController {
 
     private final Organization organization;
@@ -24,16 +26,19 @@ public class OrganizationController {
 
     @PostMapping(value = "/organization", consumes = "application/json")
     public void setOrganization(@RequestBody final Map<String, String> organizationReceived) {
+        log.info("OrganizationController::setOrganization");
         organization.addEmployees(organizationReceived);
     }
 
     @GetMapping(value = "/organization", produces = "application/json")
     public Map<String, Map> getOrganization() {
+        log.info("OrganizationController::getOrganization");
         return downstreamHierarchy(organization.getRoot().map(List::of).orElse(List.of()));
     }
 
     @GetMapping(value = "/organization/employee/{employeeName}/management", produces = "application/json")
     public Map<String, Map> getEmployee(@PathVariable(value = "employeeName") String employeeName) {
+        log.info("OrganizationController::getEmployee");
         // todo.map(::upstreamHierarchy);
         return upstreamHierarchy(organization.getEmployee(employeeName).get());
     }
